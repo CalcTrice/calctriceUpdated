0683c58213e58952a84d92333780595e41cf3a0a
CalcTrice <mycalctrice@gmail.com>
2021-04-06 19:09:21 +0530

    2 sections from landing page completed

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-// const loadingTl = gsap.timeline();
-// loadingTl
-
-
-
-
 const navToggler = function () {
     const navIcon = document.querySelector('.burger');
     const closeNav = document.querySelector('.close-nav');
     const navLinks = document.querySelector('.nav-links');
+    const blurSlate = document.querySelector('.blurSlate');
+    const logo = document.querySelector('.logo');
 
     let tl = gsap.timeline();
 
+    blurSlate.addEventListener('click', function () {
+        navLinks.classList.remove('show');
+        tl.to('.blurSlate', { scaleX: 0, duration: .4, transformOrigin: 'right' })
+    });
+
     navIcon.addEventListener('click', function () {
         navLinks.classList.add('show');
-        tl.to('.blurSlate', { scaleX: 0.75, duration: 0.7, transformOrigin: 'left' }, "=-0.2")
+        tl
+            .to('.blurSlate', { scaleX: 1, duration: 1, transformOrigin: 'left' }, "=-0.2")
+            .from('.blurSlate .info', { x: -200, opacity: 0, duration: 0.3 })
+
+        logo.style.transition = 'all 0.1s ease-out';
+        logo.style.opacity = 0;
     });
+
     closeNav.addEventListener('click', function () {
         navLinks.classList.remove('show');
-        tl.to('.blurSlate', { scaleX: 0, duration: .7, transformOrigin: 'left' })
+        tl
+            .from('.blurSlate .info', { opacity: 1, duration: 0.2 })
+            .to('.blurSlate', { scaleX: 0, duration: .4, transformOrigin: 'right' },)
+        logo.style.transition = 'all 0.1s ease-out';
+        logo.style.opacity = 1;
     });
 }
-navToggler()
 const homeGsap = function () {
 
     let tl = gsap.timeline({
@@ -31,22 +41,13 @@ const homeGsap = function () {
         }
     })
     tl
-        .from('.loading-text', { opacity: 0, duration: 0.4 }, "+=1")
-        .to('.loading-animation', { scaleX: 5, duration: 0.6, transformOrigin: "top " }, "+=1")
-        .to('.loading-text', { display: 'none' })
-        .to('.company-name', { display: 'block' })
-        .to('.loading-animation', { scaleX: 0, duration: 0.6, transformOrigin: "top " })
-        .to('.loader', { scaleY: 0, duration: 1, opacity: 0, ease: 'power4.out', transformOrigin: 'top' }, "+=1")
-
         .to('h1 div', { opacity: 1, y: '0%', duration: 1, stagger: 0.2, ease: 'power4.out' }, '-=0.4')
-        .to("span.heading-hover", { "-webkit-text-fill-color": 'transparent', delay: 0.1, stagger: 0.05, ease: "power4.out" })
         .from('.CTA button', { y: '100%', opacity: 0, stagger: 0.1, duration: 0.5, ease: "bounce.out", stagger: 0.1 })
         .from('.box', { opacity: 0, duration: 0.5 }, "-=0.2")
         .from("canvas", { scale: 0, opacity: 0, duration: 0.5, ease: 'power.out' }, "-=0.5")
         .from('nav', { opacity: 0, duration: 0.2 })
         .from('footer.footer-landing', { opacity: 0, duration: 0.2 }, "-=0.2")
 }
-
 const buttonAnimations = function () {
     const button1 = document.querySelector('.button-1');
     const button2 = document.querySelector('.button-2');
@@ -57,34 +58,33 @@ const buttonAnimations = function () {
     button1.addEventListener('mouseenter', function () {
         box.style.transition = "0.3s ease-out";
         box.style.left = "0%";
-        box.style.transform = "translateX(-2%)"
+        box.style.transform = "translateX(0%)"
     })
     button1.addEventListener('mouseleave', function () {
         box.style.transition = "0.3s ease-out";
         box.style.left = "50%";
-        box.style.transform = "translateX(-52%)"
+        box.style.transform = "translateX(-50%)"
     })
 
     button3.addEventListener('mouseenter', function () {
         box.style.transition = "0.3s ease-out";
         box.style.left = "100%";
-        box.style.transform = "translateX(-102%)"
+        box.style.transform = "translateX(-100%)"
     })
     button3.addEventListener('mouseleave', function () {
         box.style.transition = "0.3s ease-out";
         box.style.left = "50%";
-        box.style.transform = "translateX(-52%)"
+        box.style.transform = "translateX(-50%)"
     })
 
 
 
 }
-
 const particleJS = function () {
-    particlesJS("landingPage", {
+    particlesJS("landingSection", {
         particles: {
-            number: { value: 60, density: { enable: true, value_area: 800 } },
-            color: { value: "#FFD066" },
+            number: { value: 80, density: { enable: true, value_area: 800 } },
+            color: { value: "#000000" },
             shape: {
                 type: "circle",
                 stroke: { width: 0, color: "#000000" },
@@ -92,9 +92,9 @@ const particleJS = function () {
                 image: { src: "img/github.svg", width: 100, height: 100 }
             },
             opacity: {
-                value: 0.5,
+                value: 0.7,
                 random: false,
-                anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+                anim: { enable: false, speed: 1.5, opacity_min: 0.1, sync: false }
             },
             size: {
                 value: 3,
@@ -104,7 +104,7 @@ const particleJS = function () {
             line_linked: {
                 enable: true,
                 distance: 150,
-                color: "#ffffff",
+                color: "#444",
                 opacity: 0.4,
                 width: 1
             },
@@ -127,7 +127,7 @@ const particleJS = function () {
                 resize: true
             },
             modes: {
-                grab: { distance: 231.44200550588337, line_linked: { opacity: 1 } },
+                grab: { distance: 220, line_linked: { opacity: 1 } },
                 bubble: {
                     distance: 200,
                     size: 10,
@@ -154,7 +154,6 @@ const particleJS = function () {
     requestAnimationFrame(update);
 
 }
-
 const sideNavGsap = function () {
     const navIcon = document.querySelector('.burger');
     let tl = gsap.timeline();
@@ -165,9 +164,125 @@ const sideNavGsap = function () {
 
     })
 }
+const testimonialsCarouselSlider = function () {
+    const slider = document.querySelector('.slider');
+    const carousel = document.querySelector('.testimonials-carousel');
+    const prev = document.querySelector('.arrow-prev');
+    const next = document.querySelector('.arrow-next');
+    let direction = -1;
 
-navToggler()
-particleJS();
-homeGsap();
-sideNavGsap();
-buttonAnimations();
+    prev.addEventListener('click', function () {
+        if (direction === -1) {
+            direction = 1;
+            slider.appendChild(slider.firstElementChild);
+
+        }
+        carousel.style.justifyContent = 'flex-end'
+        slider.style.transform = 'translate(20%)';
+
+    });
+
+    next.addEventListener('click', function () {
+
+        if (direction === 1) {
+            direction = -1;
+            slider.prepend(slider.lastElementChild);
+        }
+
+        carousel.style.justifyContent = 'flex-start'
+        slider.style.transform = 'translate(-20%)';
+
+    });
+    slider.addEventListener('transitionend', function () {
+        if (direction === -1)
+            slider.appendChild(slider.firstElementChild);
+        else if (direction === 1)
+            slider.prepend(slider.lastElementChild);
+
+        slider.style.transition = 'none';
+        slider.style.transform = 'translate(0%)';
+
+        setTimeout(function () {
+
+            slider.style.transition = 'all 0.3s ease-in-out';
+        });
+    })
+}
+const testimonialSlideContentCreator = function () {
+
+    let counter = 1;
+    const content = [
+        [
+            'Great collection of tools!',
+            'Adam West',
+            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo  A sunt deleniti exercitationem? At atque',
+        ],
+        [
+            'Great collection of tools!',
+            'John Jacobs',
+            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo A sunt deleniti exercitationem? At atque ducimus omnis cum qui voluptate vero corporis molestias nemo!',
+        ],
+        [
+            'Great collection of tools!',
+            'Martha Stewart',
+            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo A sunt deleniti exercitationem?',
+        ],
+        [
+            'Great collection of tools!',
+            'Max Robbie',
+            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo Nihil quam, hnarum accusatium dolorum facilis voluptatibus.',
+        ],
+        [
+            'Great collection of tools!',
+            'Jay Cart',
+            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo Nihil quam, harum accusantium dolorum facilis voluptatibus.',
+        ],
+    ];
+
+    const slider = document.querySelector('.slider');
+
+    content.forEach(function (person, id) {
+
+        slider.insertAdjacentHTML('beforeend',
+            `<div class="slide" id="slide-${counter + id}">
+                    <div class="slide-content">
+                        <div>
+                            <q>${person[0]}</q>
+                            
+                            <span class="username">${person[1]}</span>
+                        </div>
+                        <blockquote>"${person[2]}"</blockquote>
+                    </div>
+                </div>`
+        );
+
+    });
+}
+const navIntersection = function () {
+    const nav = document.querySelector('nav');
+    const section2 = document.querySelector('.section-2');
+    const section4 = document.querySelector('.section-4');
+
+    const obsCallback = function (entries, observer) {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting == true)
+                console.log('Hello');
+        })
+
+    }
+    const obsOptions = {
+        root: section2,
+        threshold: 0.7
+    }
+    const observer = new IntersectionObserver(obsCallback, obsOptions);
+    observer.observe(nav);
+}
+window.onload = function () {
+    navToggler()
+    particleJS();
+    homeGsap();
+    sideNavGsap();
+    buttonAnimations();
+    testimonialsCarouselSlider();
+    testimonialSlideContentCreator();
+}
